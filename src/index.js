6661c445e58ea8afc40943cb9371494d6c520d29
Josh Marinacci <joshua@marinacci.org>
2019-07-11 11:27:19 -0700

    import more code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react'
 import {Dialog} from './Dialog'
-
+export TreeTable from './TreeTable'
 export {Dialog} from './Dialog'
+export {Spacer, ToggleButton, MenuPopup} from "./GridEditorApp"
+export QRCanvas from "./QRCanvas"
+export InputManager from "./InputManager"
+export SelectionManager, {SELECTION_MANAGER} from "./SelectionManager"
+export PropSheet, {TYPES as PROP_TYPES} from "./PropSheet"
+export TreeItemProvider, {getLoginURL, TREE_ITEM_PROVIDER, getDocsURL, getScriptsURL, getAssetsURL, getInfoURL, getUserURL} from "./TreeItemProvider"
+
+export {toQueryString, makePoint, setQuery, parseOptions, on, genID} from "./utils"
+
 export default class extends Component {
   render() {
     return <div>
