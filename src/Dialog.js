1af2e41cdf9dd061ff490fb163cd0e096615d60b
Josh Marinacci <joshua@marinacci.org>
2019-07-11 10:29:20 -0700

    added dialog class

---
+++ src/Dialog.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import '../css/dialogs.css'
+export class Dialog extends Component {
+    clickedScrim = (e) => {
+        if(e.target !== this.scrim) return
+        if(this.props.onScrimClick) this.props.onScrimClick(e)
+    }
+    render() {
+        if(!this.props.visible) return <div/>;
+        const bodyStyle = {}
+        if(this.props.width) bodyStyle.width = this.props.width
+        if(this.props.height) bodyStyle.height = this.props.height
+        return <div className={"dialog-scrim"} ref={(scrim)=>this.scrim=scrim} onClick={this.clickedScrim}>
+            <div className={"dialog-body"} style={bodyStyle}>{this.props.children}</div>
+        </div>
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react'
+import {Dialog} from './Dialog'
 
+export {Dialog} from './Dialog'
 export default class extends Component {
   render() {
     return <div>
-      <h2>Welcome to React components</h2>
+      <h2>This is the editor</h2>
+      <Dialog visible={true}>
+        <header>dialog header</header>
+        <div>dialog body</div>
+        <footer>dialog footer</footer>
+      </Dialog>
     </div>
   }
 }
