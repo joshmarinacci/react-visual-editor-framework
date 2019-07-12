bf3e83f9c54604c2bf7db56600968f2d3693a6e2
Josh Marinacci <joshua@marinacci.org>
2019-07-11 19:37:31 -0700

    updates

--- demo/src/RectDocApp.js
+++ demo/src/RectDocApp.js
@@ -3,6 +3,8 @@ import "../../css/grid.css"
 import TreeTable from '../../src/TreeTable'
 import PropSheet from '../../src/PropSheet'
 import {Spacer} from 'appy-comps'
+import SelectionManager, {SELECTION_MANAGER} from '../../src/SelectionManager'
+import {TREE_ITEM_PROVIDER} from '../../src/TreeItemProvider'
 export class RectDocApp extends Component {
     constructor(props) {
         super(props)
@@ -45,7 +47,7 @@ export class RectDocApp extends Component {
             <Resizer onMouseDown={this.resizeLeft}/>
 
             <div className="panel">
-                <div>center canvas</div>
+                <RectCanvas provider={this.props.provider}/>
             </div>
 
             <Resizer onMouseDown={this.resizeRight}/>
@@ -58,9 +60,11 @@ export class RectDocApp extends Component {
 
             <Resizer onMouseDown={this.resizeLeft}/>
             <div className="toolbar">
+                <button onClick={this.toggleLeftPane}>toggle left</button>
                 <Spacer/>
                 <label>status bar</label>
                 <Spacer/>
+                <button onClick={this.toggleRightPane}>toggle right</button>
             </div>
             <Resizer onMouseDown={this.resizeRight}/>
 
@@ -68,8 +72,96 @@ export class RectDocApp extends Component {
 
     }
 
+
+    //event handlers
+    resizeLeft = (e) => {
+        window.addEventListener('mousemove',this.resizeLeftWindow)
+        window.addEventListener('mouseup', this.endResizeLeftWindow)
+    }
+    resizeLeftWindow =(e) => {
+        this.setState({leftDivider:e.clientX+'px'})
+    }
+    endResizeLeftWindow = (e) => {
+        window.removeEventListener('mousemove', this.resizeLeftWindow)
+        window.removeEventListener('mouseup', this.endResizeLeftWindow)
+    }
+
+
+    resizeRight = (e) => {
+        window.addEventListener('mousemove', this.resizeRightWindow)
+        window.addEventListener('mouseup', this.endResizeRightWindow)
+    }
+    resizeRightWindow = (e)=>{
+        const size = document.querySelector('.grid').getBoundingClientRect()
+        this.setState({rightDivider:(size.width-e.clientX)+'px'})
+    }
+    endResizeRightWindow = (e) => {
+        window.removeEventListener('mousemove', this.resizeRightWindow)
+        window.removeEventListener('mouseup', this.endResizeRightWindow)
+    }
+
+    toggleLeftPane = (e) => {
+        if(this.state.leftDivider === '0px') {
+            this.setState({leftDivider:'250px'})
+        } else {
+            this.setState({leftDivider:'0px'})
+        }
+    }
+    toggleRightPane = (e) => {
+        if(this.state.rightDivider === '0px') {
+            this.setState({rightDivider:'250px'})
+        } else {
+            this.setState({rightDivider:'0px'})
+        }
+    }
+    toggleBottomPane = (e) => {
+        if(this.state.bottomDivider === '0px') {
+            this.setState({bottomDivider:'250px'})
+        } else {
+            this.setState({bottomDivider:'0px'})
+        }
+    }
+
 }
 
 const Resizer = (props) => {
     return <div className="resizer" {...props}/>
+}
+
+
+class RectCanvas extends Component {
+    componentWillMount() {
+        console.log('this.props.provider',this.props.provider)
+        SelectionManager.on(SELECTION_MANAGER.CHANGED, this.redraw)
+        this.props.provider.on(TREE_ITEM_PROVIDER.STRUCTURE_CHANGED, this.redraw)
+        this.props.provider.on(TREE_ITEM_PROVIDER.PROPERTY_CHANGED, this.redraw)
+    }
+    componentDidMount() {
+        this.redraw()
+    }
+
+    redraw = (e) => {
+        if(!this.canvas) return
+        const c = this.canvas.getContext('2d')
+        c.fillStyle = 'white'
+        c.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        c.save()
+        this.props.provider.getSceneRoot().children.forEach(ch => {
+            c.fillStyle = ch.color
+            c.fillRect(ch.x,ch.y,ch.w,ch.h)
+        })
+        c.restore()
+    }
+
+    render() {
+        return <div className="panel">
+        <canvas style={{border: '1px solid red', width:'300px', height:'300px'}}
+                    width={300} height={300} ref={(e) => this.canvas = e}
+                    onClick={this.onClick}
+                    onMouseDown={this.mouseDown}
+                    onMouseUp={this.mouseUp}
+                    onMouseMove={this.mouseMove}
+            />
+        </div>
+    }
 }
--- demo/src/RectDocEditor.js
+++ demo/src/RectDocEditor.js
@@ -1,4 +1,4 @@
-import TreeItemProvider from '../../src/TreeItemProvider'
+import TreeItemProvider, {TREE_ITEM_PROVIDER} from '../../src/TreeItemProvider'
 import React from 'react'
 import {RectDocApp} from './RectDocApp'
 import {PROP_TYPES} from '../../src'
@@ -8,12 +8,20 @@ export default class RectDocEditor extends TreeItemProvider {
         super(options)
         this.root = this.makeEmptyRoot()
     }
-    getApp() {
-        return <RectDocApp provider={this}/>
-    }
 
     makeEmptyRoot(doc) {
         const root = {id:'root',type:'root',children:[]}
+        const square1 = {
+            type:'square',
+            id:'sq1',
+            x:100,
+            y:100,
+            w:100,
+            h:100,
+            color:'blue',
+        }
+        root.children.push(square1)
+        return root
     }
 
     getSceneRoot() {
@@ -22,7 +30,6 @@ export default class RectDocEditor extends TreeItemProvider {
 
     getProperties(item) {
         let defs = []
-        console.log("looking at item",item)
         if(!item) return defs;
         defs.push({
             key:'id',
@@ -31,6 +38,33 @@ export default class RectDocEditor extends TreeItemProvider {
             locked:true,
             value:item.id
         })
+        if(item.type === 'square') {
+            defs.push({key:'x',name:'X',type:PROP_TYPES.NUMBER, value:item.x})
+            defs.push({key:'y',name:'Y',type:PROP_TYPES.NUMBER, value:item.y})
+            defs.push({key:'w',name:'Width',type:PROP_TYPES.NUMBER, value:item.w})
+            defs.push({key:'h',name:'Height',type:PROP_TYPES.NUMBER, value:item.h})
+        }
+
         return defs;
+    }
+
+    setPropertyValue(item, def, value) {
+        item[def.key] = value
+        this.fire(TREE_ITEM_PROVIDER.PROPERTY_CHANGED,{
+            provider: this,
+            child:item,
+            propKey:def.key,
+            oldValue:def.value,
+            newValue:value
+        })
+    }
+
+
+    getRendererForItem(item) {
+        return <label>{item.type} {item.id}</label>
+    }
+
+    getApp() {
+        return <RectDocApp provider={this}/>
     }
 }
